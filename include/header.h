7b306f6d8882c5306376332be1e1a8c0bef8dce7
Yuri Reinaldo Da Silva <yuri.wrlk@A303-12L.imd.local>
2018-03-24 10:09:50 -0300

    Criando estrutura base do projeto

---
+++ include/header.h
@@ -0,0 +1,15 @@
+/*!
+ *	@file header.h
+ *
+ *  File with all the standard libraries needed to be include.
+ */
+#ifndef _HEADER_
+#define _HEADER_
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stdexcept>
+#include <new>
+
+#endif
